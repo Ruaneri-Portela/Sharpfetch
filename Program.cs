e9bac1b2ebae8f7e3dd19ef6b8340dc0426f7e5c
Ruaneri Portela <ruaneriportela@outlook.com>
2023-10-17 00:19:40 -0300

    Linux Debian Add

--- Program.cs
+++ Program.cs
@@ -25,6 +25,7 @@ namespace SharpFetch
             localList.Add(data.texts[0] + osPlatform);
             localList.Add(data.texts[1] + systemArch);
             localList.Add(data.texts[2] + formatedTime);
+            Console.WriteLine(osPlatform);
             if (osPlatform.Contains("Windows"))
             {
                 List<string> mergeList = localList.Concat(FetchWindows()).ToList();
@@ -54,16 +55,58 @@ namespace SharpFetch
                     index++;
                 }
             }
+            else if (osPlatform.Contains("Debian"))
+            {
+                // Get version
+                string[] version = GenericQuery("lsb_release", "-d").Split("\n");
+                int debianIndex = version[0].IndexOf("Debian");
+                localList[2] = data.texts[0] + version[0].Substring(debianIndex).Trim();
+                // Get Packges
+                string[] parts = GenericQuery("dpkg", "--get-selections").Split("\n");
+                localList.Add(data.texts[5] + (parts.Length - 1));
+                // Merge with linux communs data
+                List<string> mergeList = localList.Concat(FetchLinux()).ToList();
+                int index = 0;
+                int size = mergeList.Count - 1;
+                SplashData splash = new SplashData(1);
+                foreach (string line in splash.asciiArt)
+                {
+                    Console.Write(line + "    ");
+                    if (size >= index)
+                    {
+                        Console.Write(mergeList[index]);
+                    }
+                    else if (size + 1 >= index)
+                    {
+                        Console.Write("");
+                    }
+                    else if (size + 2 >= index)
+                    {
+                        DarkBar();
+                    }
+                    else if (size + 3 >= index)
+                    {
+                        Bar();
+                    };
+                    Console.WriteLine();
+                    index++;
+                }
+            }
+        }
+        static List<string> FetchLinux()
+        {
+            List<string> localList = new List<string>();
+            return localList;
         }
         static List<string> FetchWindows()
         {
             List<string> localList = new List<string>();
             localList.Add(data.texts[3] + QueryReg("HKEY_LOCAL_MACHINE\\HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0", "ProcessorNameString"));
             localList.Add(data.texts[4] + QueryReg("HKEY_LOCAL_MACHINE\\HARDWARE\\DESCRIPTION\\System\\BIOS", "SystemFamily"));
-            string[] parts = ListReg("HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\").Split("\n");
+            string[] parts = GenericQuery("reg", "query HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\").Split("\n");
             localList.Add(data.texts[5] + (parts.Length - 1));
-            string[] parts2 = QueryWmic("OS get FreePhysicalMemory, TotalVisibleMemorySize").Split("\n");
-            string[] parts3 = QueryWmic("path win32_videocontroller get caption").Split("\n");
+            string[] parts2 = GenericQuery("wmic", "OS get FreePhysicalMemory, TotalVisibleMemorySize").Split("\n");
+            string[] parts3 = GenericQuery("wmic", "path win32_videocontroller get caption").Split("\n");
             string pattern = @"\d+";
             MatchCollection matches = Regex.Matches(parts2[1], pattern);
             List<string> memory = new List<string>();
@@ -97,32 +140,13 @@ namespace SharpFetch
             };
             return query;
         }
-        static string ListReg(string value)
-        {
-            string searchResult = "";
-            ProcessStartInfo psi = new ProcessStartInfo
-            {
-                FileName = "reg.exe",
-                Arguments = $"query {value}",
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                CreateNoWindow = true
-            };
-            using (Process process = new Process { StartInfo = psi })
-            {
-                process.Start();
-                searchResult = process.StandardOutput.ReadToEnd();
-                process.WaitForExit();
-            };
-            return searchResult;
-        }
-        static string QueryWmic(string value)
+        static string GenericQuery(string exec, string args)
         {
             string queryReturn = "";
             ProcessStartInfo psi = new ProcessStartInfo
             {
-                FileName = "wmic.exe",
-                Arguments = $"{value}",
+                FileName = exec,
+                Arguments = args,
                 UseShellExecute = false,
                 RedirectStandardOutput = true,
                 CreateNoWindow = true
